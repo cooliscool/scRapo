d73aa1f3fb9776bac160cb7c128303e1b2c401a0
sasebot <sasebot@gmail.com>
2017-07-12 23:43:35 -0400

    new

--- JSONparser01.py
+++ JSONparser01.py
@@ -22,12 +22,12 @@ if __name__ == '__main__':
 	isSearch = int (jsondata['search'])
 	targetRoot = jsondata['targetRoot']
 
-	if isSearch :
-		searchLink = jsondata['searchQuery'] + jsondata['searchSeed']
-		rootPage = requests.get(searchLink, headers = reqHeaders).text
-
-	else :
-		rootPage = requests.get(targetRoot, headers = reqHeaders).text
+	# if isSearch == 1 :
+	# 	searchLink = jsondata['searchQuery'] + jsondata['searchSeed']
+	# 	rootPage = requests.get(searchLink, headers = reqHeaders).text
+	#
+	# else :
+	# 	rootPage = requests.get(targetRoot, headers = reqHeaders).text
 
 	# Start the while loop from parsing jobs
 
@@ -35,4 +35,62 @@ if __name__ == '__main__':
 
 		jobs = jsondata['do']
 
-		
+		if len(jobs) > 0 :
+			for job in jobs:
+
+				# Get the requirements and type of job
+
+				doRepeatedly = int(job['repeated'])
+				isThereASubtask = int(job['isThereASubtask'])
+				wannaSave = int(job['saveThis'])
+				name = job['name']
+
+				# TODO : say what is page
+
+				#
+				# Parsing the data, whatever it is .
+				#
+
+				tup = (page,)
+				for q in job['afterSequence'] :
+					tup = tup + (q,)
+				tup += ( job['bet'] , job['ween'], )
+
+				off = 0
+				ln = len(page)
+
+				dat = []
+
+					# Scrap !
+
+				while off < ln:
+					try :
+						val, off = getContent(tup, offset= off)
+						dat.append(val)
+						if doRepeatedly == 0:
+							break
+					except ValueError:
+						break
+
+				print (dat) # Log scraped things4
+
+				# If there is a subtask for the corresponding scrap,
+				# add each link and its job to queue.
+				#
+
+				if isThereASubtask == 1 :
+					# TODO: define a queue and level too
+
+					for link in dat :
+						queue.append( {level, link , job['do']})
+
+				#
+				# Save or not , depending on wannaSave
+				#
+
+				if wannaSave == 1:
+
+					# TODO : define a dictionary of all scrap data
+
+					for d in dat :
+						dictionary.add({ ,name , d})
